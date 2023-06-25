27c8a318ba45cb02e1237ebf62f738a40142f35b
roman krasilnikov <krasilnikovroman5@gmail.com>
2023-06-25 22:45:11 +0700

    search bar

---
+++ components/ServiceList.tsx
@@ -0,0 +1,14 @@
+"use client";
+import styles from '@/styles/components/ServiceList.module.scss';
+import {PriceList} from "@/app/services/PriceList"
+import SerachBar from './SerachBar';
+import {useState} from 'react';
+import { priceList } from '@/types/priceList';
+
+const ServiceList = () =>{
+    
+    return(
+       <>
+       </>
+    )
+}
